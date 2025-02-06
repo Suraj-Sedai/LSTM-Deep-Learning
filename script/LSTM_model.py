efcc098ce02d883317b3dd09848d10e825476da4
Suraj Sedai <surajsedai05@gmail.com>
2025-02-05 23:30:14 -0500

    break

--- script/LSTM_model.py
+++ script/LSTM_model.py
@@ -13,18 +13,18 @@ for i in range(len(data) - 1):
     X.append(data[i])
     y.append(data[i + 1])
 
-# Reshape the data to (samples, time_steps, features)
+# Reshape the data to (samples, time_steps, features) 
 X = np.array(X).reshape((len(X), 1, 1))
 y = np.array(y)
 
 # Step 2: Build the LSTM Model
 model = Sequential()
-model.add(LSTM(50, activation='relu', input_shape=(1, 1)))  # LSTM layer
+model.add(LSTM(50, activation='tanh', input_shape=(1, 1)))  # LSTM layer
 model.add(Dense(1))  # Output layer
 model.compile(optimizer='adam', loss='mse')  # Compile the model
 
 # Step 3: Train the Model
-model.fit(X, y, epochs=200, verbose=0)
+model.fit(X, y, epochs=1000, verbose=0)
 
 # Step 4: Make Predictions
 prediction = model.predict(np.array([[70]]).reshape(1, 1, 1))
